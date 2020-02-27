edeb9ebca48683f10c0e2db2882e514fa3d33d81
Saren Inden <saren@sareninden.nl>
2020-02-27 19:07:58 +0100

    Added support files for auto layout and other minor supporting functions

--- Weather/Weather/Modules/Main/MainViewController.swift
+++ Weather/Weather/Modules/Main/MainViewController.swift
@@ -1,11 +1,20 @@
 import UIKit
 
 internal final class MainViewController: UIViewController {
+    private lazy var rootView = View()
+    
     internal init() {
         super.init(nibName: nil, bundle: nil)
     }
-    
-    required init?(coder: NSCoder) {
+
+    internal required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: Load view
+extension MainViewController {
+    internal override func loadView() {
+        view = rootView
     }
 }
