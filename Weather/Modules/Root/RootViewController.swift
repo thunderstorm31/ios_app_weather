ca29671ab229ae1be34df4811bd98a2c72a40e7d
Saren Inden <saren@sareninden.nl>
2020-02-29 21:39:44 +0100

    Updated keyboard inset

--- Weather/Modules/Root/RootViewController.swift
+++ Weather/Modules/Root/RootViewController.swift
@@ -101,6 +101,10 @@ extension RootViewController {
         let width = min(450, min(bounds.height, bounds.width) - 60)
         
         leadingContainerView.pin(width: width)
+        
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.pannedLeadingContainer(_:)))
+        
+        leadingContainerView.addGestureRecognizer(panRecognizer)
     }
     
     private func configureTrailingContainerView() {
@@ -114,20 +118,31 @@ extension RootViewController {
         
         trailingContainerView.pin(width: width)
         trailingContainerView.isHidden = true
+        
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.pannedTrailingContainer(_:)))
+        
+        trailingContainerView.addGestureRecognizer(panRecognizer)
     }
 }
 
 // MARK: - Display mode
 extension RootViewController {
+    private var leadingXTransform: CGFloat { -(leadingContainerView.bounds.width + 50) }
+    private var trailingXTransform: CGFloat { trailingContainerView.bounds.width + 50 }
+    
     private func setLeadingContainerHiddenTransform() {
-        leadingContainerView.transform = CGAffineTransform(translationX: -(leadingContainerView.bounds.width + 50), y: 0)
+        leadingContainerView.transform = CGAffineTransform(translationX: leadingXTransform, y: 0)
     }
     
     private func setTrailingContainerHiddenTransform() {
-        trailingContainerView.transform = CGAffineTransform(translationX: trailingContainerView.bounds.width + 50, y: 0)
+        trailingContainerView.transform = CGAffineTransform(translationX: trailingXTransform, y: 0)
     }
     
     internal func setDisplayMode(_ displayMode: DisplayMode, animated: Bool, completion: (() -> Void)? = nil) {
+        setDisplayMode(displayMode, animated: animated, animationDuration: 0.25, completion: completion)
+    }
+    
+    private func setDisplayMode(_ displayMode: DisplayMode, animated: Bool, animationDuration: TimeInterval, completion: (() -> Void)? = nil) {
         guard self.displayMode != displayMode else {
             return
         }
@@ -246,5 +261,57 @@ extension RootViewController {
     @objc
     private func tappedOverlay(_ sender: UITapGestureRecognizer) {
         setDisplayMode(.main, animated: true)
+    }
+    
+    @objc
+    private func pannedLeadingContainer(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .changed:
+            let translation = sender.translation(in: view)
+            
+            leadingContainerView.transform = CGAffineTransform(translationX: min(0, translation.x), y: 0)
+        case .ended:
+            let translation = sender.translation(in: view)
+            let containerWidth = leadingContainerView.bounds.width
+            
+            if -translation.x > 0.5 * containerWidth || sender.velocity(in: view).x < -1500 {
+                let percentage = min(1, max(abs(translation.x) / containerWidth, 0))
+                let animationDuration: TimeInterval = 0.25 * TimeInterval(1 - percentage)
+                
+                setDisplayMode(.main, animated: true, animationDuration: animationDuration)
+            } else {
+                UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseOut, animations: {
+                    self.leadingContainerView.transform = .identity
+                }, completion: nil)
+            }
+        default:
+            break
+        }
+    }
+    
+    @objc
+    private func pannedTrailingContainer(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .changed:
+            let translation = sender.translation(in: view)
+            
+            trailingContainerView.transform = CGAffineTransform(translationX: max(0, translation.x), y: 0)
+        case .ended:
+            let translation = sender.translation(in: view)
+            let containerWidth = trailingContainerView.bounds.width
+                        
+            if translation.x > 0.5 * containerWidth || sender.velocity(in: view).x > 1500 {
+                let percentage = min(1, max(abs(translation.x) / containerWidth, 0))
+                let animationDuration: TimeInterval = 0.25 * TimeInterval(1 - percentage)
+                
+                setDisplayMode(.main, animated: true, animationDuration: animationDuration)
+            } else {
+                UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseOut, animations: {
+                    self.trailingContainerView.transform = .identity
+                }, completion: nil)
+            }
+        default:
+            break
+        }
     }
 }
